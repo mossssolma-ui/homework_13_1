381e34840991bebb77ff91e81ba7da474d17f413
Oleg Mat <mos_olma@mail.ru>
2025-11-13 22:03:34 +0300

    fixes files to lint

--- src/csv_excel.py
+++ src/csv_excel.py
@@ -1,29 +1,31 @@
-import pandas as pd
+from typing import Any
+
+import pandas as pd  # type: ignore
 
 
-def get_convert_csv_to_list_dict(file_csv: str) -> list[dict]:
+def get_convert_csv_to_list_dict(file_csv: str) -> list[dict[Any, Any]]:
     """
     Функция конвертирует файл csv в список словарей
     """
     try:
         csv_df = pd.read_csv(file_csv, sep=";", encoding="utf-8")
-        transactions = csv_df.to_dict(orient='records')
+        transactions: list[dict[Any, Any]] = csv_df.to_dict(orient="records")
         return transactions
-    except FileNotFoundError as e:
+    except FileNotFoundError:
         return []
-    except Exception as e:
+    except Exception:
         return []
 
 
-def get_convert_excel_to_list_dict(file_excel: str) -> list[dict]:
+def get_convert_excel_to_list_dict(file_excel: str) -> list[dict[Any, Any]]:
     """
     Функция конвертирует файл excel в список словарей
     """
     try:
         excel_df = pd.read_excel(file_excel)
-        transactions = excel_df.to_dict(orient="records")
+        transactions: list[dict[Any, Any]] = excel_df.to_dict(orient="records")
         return transactions
-    except FileNotFoundError as e:
+    except FileNotFoundError:
         return []
-    except Exception as e:
+    except Exception:
         return []
